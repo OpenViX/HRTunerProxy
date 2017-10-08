e146d3ce1adfd18e39e3ebb7817c6d0bb7fbf668
Andrew Blackburn <andyblac@icloud.com>
2017-10-08 01:30:40 +0100

    update to read version from PLUGIN_VERSION created file during build.

--- plugin/about.py
+++ plugin/about.py
@@ -8,13 +8,21 @@ from Components.config import config
 from Components.Label import Label
 from Components.Pixmap import Pixmap
 from Components.Sources.StaticText import StaticText
+from os import path
 
-PLUGIN_VERSION = '3.1'
+def getVersion():
+	if path.exists("/usr/lib/enigma2/python/Plugins/SystemPlugins/PlexDVRAPI/PLUGIN_VERSION"):
+		f = open("/usr/lib/enigma2/python/Plugins/SystemPlugins/PlexDVRAPI/PLUGIN_VERSION")
+		PLUGIN_VERSION = f.read().replace('\n','')
+		f.close()
+	else:
+		PLUGIN_VERSION = _('unknown')
+	return PLUGIN_VERSION
 
 class PlexDVRAPI_About(Screen):
 	skin="""
-	<screen position="center,center" size="600,500">
-		<widget name="about" position="10,10" size="580,430" font="Regular;22"/>
+	<screen position="center,center" size="500,500">
+		<widget name="about" position="10,10" size="480,430" font="Regular;22"/>
 		<widget name="key_red" position="0,460" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1"/>
 		<ePixmap pixmap="skin_default/buttons/red.png" position="0,460" size="140,40" alphatest="on"/>
 	</screen>"""
@@ -47,15 +55,16 @@ class PlexDVRAPI_About(Screen):
 
 		self["key_red"] = Button(_("Close"))
 
-		credit = "Plex DVR API for Enigma2 %s (c) 2017 \nAndrew Blackburn & Rowland Huevos\n" % PLUGIN_VERSION
-		credit += "http://github.com/openvix\n"
-		credit += "Application credits:\n"
+		credit = _("Plex DVR API for Enigma2 v%s (c) 2017 \n") % getVersion()
+		credit += "Andrew Blackburn & Rowland Huevos\n"
+		credit += "http://github.com/openvix\n\n"
+		credit += _("Application credits:\n")
 		credit += "- AndyBlac (main developer)\n"
 		credit += "- Huevos (main developer)\n"
 		credit += "- Rossi2000 (developer)\n\n"
-		credit += "Sources credits:\n"
+		credit += _("Sources credits:\n")
 		credit += "- FidoFuz (helped us with JSON tags)\n\n"
-		credit += "Translation credits:\n"
+		credit += _("Translation credits:\n")
 		credit += "- patrickf95 / captain (German)\n"
 		credit += "- PiGeonCZ (Czech)\n"
 		credit += "- Pakorro (Spanish)"
