82bfacc2b14a643039a1ce962dc147feec628643
Andrew Blackburn <andyblac@icloud.com>
2018-01-22 10:05:59 +0000

    bump to version 3.5

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import setup_translate
 
 pkg = 'SystemPlugins.HRTunerProxy'
 setup (name = 'enigma2-plugin-systemplugins-hrtunerproxy',
-       version = '3.0',
+       version = '3.5',
        description = 'HRTunerProxy',
+       long_description = 'Setup Enigma2 to act as HR-Tuner Proxy',
+       author='AndyBlac & Huevos',
+       url='https://github.com/OpenViX/HRTunerProxy',
        package_dir = {pkg: 'plugin'},
        packages = [pkg],
-       package_data = {pkg: 
+       package_data = {pkg:
            ['plugin.png', 'plugin-hd.png', 'locale/*/LC_MESSAGES/*.mo', 'locale/*/LC_MESSAGES/*.po']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
